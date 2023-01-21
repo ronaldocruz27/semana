fa777de652d9cb3d4ebe2eb435bafb59fe2cf04b
ronaldocruz27 <ronaldosilvacruz@gmail.com>
2023-01-21 16:58:09 -0300

    primeiro commit

---
+++ Semana.java
@@ -0,0 +1,42 @@
+package semana;
+
+import java.util.Scanner;
+
+public class Semana {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        int num = ler.nextInt();
+
+        switch (num) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda-feira");
+                break;
+            case 3:
+                System.out.println("Terça-feira");
+                break;
+            case 4:
+                System.out.println("Quarta-feira");
+                break;
+            case 5:
+                System.out.println("Quinta-feira");
+                break;
+            case 6:
+                System.out.println("Sexta-feira");
+                break;
+            case 7:
+                System.out.println("Sábado");
+            default:
+
+                if (num > 7) {
+                    System.out.println("Não existe dia da semana para esse número");
+                }
+        }
+
+    }
+
+}
